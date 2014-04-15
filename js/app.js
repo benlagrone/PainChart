ddc1f46bd690c6655bc07d75fee570b4102124d3
BenLaGrone <benjaminlagrone@gmail.com>
2014-04-15 13:25:35 -0500

    added files

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var paper = Raphael(" container", 500, 300);
+var dot = paper.circle( 250, 150, 100). attr({
+    fill: "# FF0000",
+    stroke: "# 000099",
+    "stroke-width": 3
+});
+
---
+++ server.js
@@ -0,0 +1,15 @@
+/**
+ * Created by benjaminscottlagrone on 4/10/14.
+ */
+var express = require('express');
+var app = express();
+
+app.use("/", express.static(__dirname));
+
+app.get('/hello.txt', function(req, res){
+    res.send('Hello World');
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
